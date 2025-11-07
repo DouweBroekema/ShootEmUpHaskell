69fddf4e20ee3f6be3bdc976a97e32754b59817b
DouweBroekema <Douwe.broekema@gmail.com>
2025-11-07 15:59:33 +0100

    Opening a window

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import Graphics.Gloss
+
+window :: Display
+window = InWindow "Game window" (500, 500) (10, 10)
+
+background :: Color
+background = blue
+
+drawing :: Picture
+drawing = circle 80
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = display window background drawing
